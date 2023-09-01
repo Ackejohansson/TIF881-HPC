fa7c8e944a988509eb81e7de3a411deca76267e9
Axel <axelgbg@live.se>
2023-09-01 10:37:34 +0200

    Finished the assignment

--- A0/sumofbillion.c
+++ A0/sumofbillion.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <time.h>
 
 int main() {
     long long int sum = 0;
     int billion = 1000000000;
-
-    for (int i = 0; i < billion; i++){
+    
+    clock_t start_time = clock();
+    for (int i = 0; i <= billion; i++){
         sum += i;
     }
+    clock_t end_time = clock();
+
+    printf("Execution time: %f seconds\n", (double)(end_time-start_time)/CLOCKS_PER_SEC);
     fprintf(stdout, "%lld\n", sum);
     return 0;
 }
