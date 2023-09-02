0a918e9a7bca6ed81d5656b6ce118aca28d660f8
Axel <axelgbg@live.se>
2023-09-03 01:22:49 +0200

    Updated: Error message, no cast on malloc

--- A0/writereadfile.c
+++ A0/writereadfile.c
@@ -3,20 +3,19 @@
 #include <assert.h>
 
 #define MATRIX_FILENAME "matrix.txt"
+#define SIZE 10 // Matrix size
 
 // Function prototypes
 FILE* open_file(const char *mode);
-int* allocate_matrix(int size);
-void create_matrix(int size);
-void read_matrix(int size);
+int* allocate_matrix();
+void create_matrix();
+void read_matrix();
 
 int main() {
-    const int matrix_size = 10;
-    create_matrix(matrix_size);
-    read_matrix(matrix_size);
+    create_matrix();
+    read_matrix();
 
     printf("Program executed successfully!\n");
-    
     return 0;
 }
 
@@ -26,45 +25,44 @@ FILE* open_file(const char *mode) {
     FILE *fp = fopen(MATRIX_FILENAME, mode);
     if (fp == NULL) {
         perror("Error opening file");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     return fp;
 }
 
-int* allocate_matrix(int size) {
-    int* matrix = (int*)malloc(sizeof(int) * size * size);
+int* allocate_matrix() {
+    int* matrix = malloc(sizeof(int) * SIZE * SIZE);
     if (matrix == NULL) {
         perror("Memory allocation error");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     return matrix;
 }
 
-void create_matrix(int size) {
-    int* matrix = allocate_matrix(size);
+void create_matrix() {
+    int* matrix = allocate_matrix();
     
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            matrix[i * size + j] = i * j;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            matrix[i * SIZE + j] = i * j;
         }
     }
     
     FILE* fp = open_file("wb");
-    fwrite(matrix, sizeof(int), size * size, fp);
+    fwrite(matrix, sizeof(int), SIZE * SIZE, fp);
     fclose(fp);
-    
     free(matrix);
 }
 
-void read_matrix(int size) {
+void read_matrix() {
     FILE* fp = open_file("rb");
-    int* matrix = allocate_matrix(size);
-    fread(matrix, sizeof(int), size * size, fp);
+    int* matrix = allocate_matrix(SIZE);
+    fread(matrix, sizeof(int), SIZE * SIZE, fp);
     fclose(fp);
     
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            assert(matrix[i * size + j] == i * j);
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            assert(matrix[i * SIZE + j] == i * j);
         }
     }
     free(matrix);
