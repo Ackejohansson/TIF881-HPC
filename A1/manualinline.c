9fd6fae086edebbd25742108f681f7c67a76dbbb
Axel Johansson <axejoh@student.chalmers.se>
2023-09-17 23:17:39 +0200

    Added task locality

--- A1/manualinline.c
+++ A1/manualinline.c
@@ -4,15 +4,14 @@
 
 int main() {
   srand(time(NULL));
-
   const int size = 30000;
   double as_re[size], as_im[size], bs_re[size], bs_im[size], cs_re[size], cs_im[size];
 
   for (int i = 0; i < size; i++) {
-    bs_re[i] = (double)rand() / RAND_MAX;
-    bs_im[i] = (double)rand() / RAND_MAX;
-    cs_re[i] = (double)rand() / RAND_MAX;
-    cs_im[i] = (double)rand() / RAND_MAX;
+    bs_re[i] = (double)rand()/RAND_MAX;
+    bs_im[i] = (double)rand()/RAND_MAX;
+    cs_re[i] = (double)rand()/RAND_MAX;
+    cs_im[i] = (double)rand()/RAND_MAX;
   }
 
   for (int i = 0; i < size; i++) {
