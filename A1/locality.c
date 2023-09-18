085ae51ab375d8a90c7956c644017e84eb5dfa90
Axel Johansson <axejoh@student.chalmers.se>
2023-09-18 19:56:08 +0200

    Changed for hyuperfine benchmark

--- A1/locality.c
+++ A1/locality.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 
 #define ROWS 1000
 #define COLS 1000
@@ -15,14 +14,14 @@ void row_sums(double * sums, const double ** matrix, size_t nrs, size_t ncs){
   }
 }
 
-void col_sums(double * sums, const double ** matrix, size_t nrs, size_t ncs){
+/*void col_sums(double * sums, const double ** matrix, size_t nrs, size_t ncs){
   for ( size_t jx = 0; jx < ncs; ++jx ) {
     double sum = 0.;
     for ( size_t ix = 0; ix < nrs; ++ix )
       sum += matrix[ix][jx];
     sums[jx] = sum;
   }
-}
+}*/
 
 int main(){
   double *asentries = (double*) malloc(ROWS * COLS * sizeof(double*));
@@ -43,26 +42,15 @@ int main(){
   }
 
   double rowsums[ROWS];
-  double colsums[COLS];
-  clock_t start_time, end_time;
-
-
-  // Run rows
-  start_time = clock();
-  row_sums(rowsums, (const double **)matrix, ROWS, COLS);
-  end_time = clock();
-  printf("Time for row_sums: %lf\n", (double)(end_time - start_time)/CLOCKS_PER_SEC);
-
-  // Run cols
-  start_time = clock();
-  col_sums(colsums, (const double **)matrix, ROWS, COLS);
-  end_time = clock();
-  printf("Time for col_sums: %lf\n", (double)(end_time - start_time)/CLOCKS_PER_SEC);
-
-  printf("Random element of rowSums: %lf\n", rowsums[500]);
-  printf("Random element of rowCols: %lf\n", colsums[500]);
-
   
+
+  row_sums(rowsums, (const double **)matrix, ROWS, COLS);
+  printf("Random element of rowSums: %lf\n", rowsums[500]);
+/*
+  double colsums[COLS];
+  col_sums(colsums, (const double **)matrix, ROWS, COLS);
+  printf("Random element of rowCols: %lf\n", colsums[500]);
+  */
   free(matrix);
   free(asentries);
   return 0;
