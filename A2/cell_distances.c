377fb434629d3c6d7ef80902048a3db402a8a7e6
Axel <axelgbg@live.se>
2023-09-27 12:10:29 +0200

    Computing distances and reads text data

--- A2/cell_distances.c
+++ A2/cell_distances.c
@@ -1,86 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <omp.h>
+#include <unistd.h>
 
-//#define SIZE 10 // Matrix size
-#define MAX_POINTS 1000000
-
-typedef struct {
-  double x, y, z;
-} Point;
-
+#define SIZE 9
 
 // Function prototypes
 int setMPThreadNumber(int argc, char *argv[], int *MP_threads);
-double distance(Point p1, Point p2);
+void read_data();
+void read_binary_data();
 
-
+///////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[]) {
-  // Set the number of threads to use
+  // Set the number of threads
   int MP_threads = -1;
   setMPThreadNumber(argc, argv, &MP_threads);
-  printf("The value after -t is: %d\n", MP_threads);
+  //omp_set_num_threads(MP_threads);
+  printf("Number of threads sat to : %d\n", MP_threads);
+  
+  // Read the data to memory
+  double x[SIZE], y[SIZE], z[SIZE];
+  printf("Reading data...\n");
+  //read_binary_data();
+  read_data(x, y, z);
+  printf("\nData read.\n");
+
+  // Compute the distances
+  printf("Computing distances...\n");
+  double distances[SIZE*SIZE/2];
+  compute_distances(distances, x, y, z);
 
 
-  // Read points from file
-  int fd = open("cells.txt", O_RDONLY);
-  if (fd == -1) {
-    perror("Error opening file");
-    return 1;
-  }
-
-  struct stat sb;
-  if (fstat(fd, &sb) == -1) {
-    perror("Error getting file size");
-    return 1;
-  }
-
-  size_t file_size = (size_t)sb.st_size;
-
-  char* file_data = (char*)mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
-  if (file_data == MAP_FAILED) {
-    perror("Error mapping file to memory");
-    return 1;
-  }
-
-  Point points[MAX_POINTS];
-  int num_points = 0;
-  char* line = file_data;
-
-  // Parallel processing using OpenMP
-  #pragma omp parallel
-  {
-    #pragma omp for
-    for (size_t i = 0; i < file_size; ++i) {
-      if (file_data[i] == '\n') {
-        if (sscanf(line, "%lf %lf %lf", &points[num_points].x, &points[num_points].y, &points[num_points].z) == 3) {
-          #pragma omp atomic
-          num_points++;
-        }
-        line = file_data + i + 1;
-      }
-    }
-  }
-
-  // Compute distances between points
-  printf("Distances between points:\n");
-  #pragma omp parallel for
-  for (int i = 0; i < num_points; i++) {
-    for (int j = i + 1; j < num_points; j++) {
-      double dist = distance(points[i], points[j]);
-      printf("Distance between point %d and point %d: %lf\n", i + 1, j + 1, dist);
-    }
-  }
-
-  // Clean up
-  munmap(file_data, file_size);
-  close(fd);
 
   return 0;
 }
@@ -94,16 +45,55 @@ int setMPThreadNumber(int argc, char *argv[], int *MP_threads) {
       *MP_threads = atoi(optarg);
 
   if (*MP_threads == -1) {
-    fprintf(stderr, "You must provide a value for -t\n");
+    fprintf(stderr, "You must provide a value for number of threads ex: -t4\n");
     exit(EXIT_FAILURE);
   }
   return 0;
 }
 
-double distance(Point p1, Point p2) {
-  double dx = p1.x - p2.x;
-  double dy = p1.y - p2.y;
-  double dz = p1.z - p2.z;
-  return sqrt(dx * dx + dy * dy + dz * dz);
+void read_data(double *x, double *y, double *z) {
+  FILE *file = fopen("./cells", "r");
+
+  if (file == NULL) {
+    perror("Error opening file");
+    exit(EXIT_FAILURE);
+  }
+
+  int i = 0;
+  while (fscanf(file, "%lf %lf %lf", &x[i], &y[i], &z[i]) == 3) {
+    i++;
+    if (i >= SIZE) {
+      printf("Warning: More than %d data points found. Array size exceeded.\n", SIZE);
+      break;
+    }
+  }
+
+  if (ferror(file)) {
+    perror("Error reading data from file");
+    fclose(file);
+    exit(EXIT_FAILURE);
+  }
+
+  fclose(file);
+
+  // Check if any data was read
+  if (i == 0) {
+    printf("No data found in the file.\n");
+    exit(EXIT_SUCCESS); // Exit successfully without an error
+  }
+
+  printf("Printing the data:\n");
+  // Process the data as needed
+  for (int j = 0; j < i; j++) {
+    printf("%f ", x[j]);
+  }
 }
 
+void compute_distances(double *distances, double *x, double *y, double *z){
+  int index = 0;
+  for (int i = 0; i < SIZE; i++)
+    for (int j = i+1; j < SIZE; j++) {
+      distances[index] = sqrt(pow(x[i]-x[j],2) + pow(y[i]-y[j],2) + pow(z[i]-z[j],2));
+      index++;
+    }
+}
