c7e32a9dbdebff49f43215903c6816e3d34b8c46
Axel <axelgbg@live.se>
2023-10-02 21:12:28 +0200

    pseudo code ich for an improved way of doing it.

--- A2/cell_distances.c
+++ A2/cell_distances.c
@@ -3,101 +3,94 @@
 #include <math.h>
 #include <omp.h>
 #include <unistd.h>
+#include <string.h>
 
-#define NR_READ 3
 #define TOT_DIST 3465
-
-// Function prototypes
-int setMPThreadNumber(int argc, char *argv[], int *MP_threads);
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[]) {
   // Set the number of threads
-  int MP_threads = 1;
-  setMPThreadNumber(argc, argv, &MP_threads);
-  printf("Number of threads sat to : %d\n", MP_threads);
+  int nr_threads = 1;
+  char* ptr = strchr(argv[1], 't');
+  if (ptr) {
+    nr_threads = strtol(++ptr, NULL, 10);
+  }
+  //omp_set_num_threads(nr_threads);
+  printf("Number of threads sat to : %d\n", nr_threads);
   
 
-  // Read the data to memory
+  // Open file
   printf("Reading data...\n");
   FILE *file = fopen("./cells", "r");
   if (file == NULL) {
     perror("Error opening file");
     exit(EXIT_FAILURE);
   }
-
-  //Compute the number of cells in the file
   fseek(file, 0, SEEK_END);
   long file_size = ftell(file);
-  const size_t cell_byte = sizeof("+00.000 -00.000 +00.000");
+  const size_t cell_byte = 24; //sizeof("+00.000 -00.000 +00.000");
   size_t nr_cells = file_size / cell_byte;
-  
-  // Allocate memory
-  double x1, y1, z1, x2, y2, z2;
-  char *block = malloc(cell_byte * nr_cells);
-  char *cell_ptr = block;
-  printf("Cell byte size: %zu\n", cell_byte);
   printf("Number of cells: %zu\n", nr_cells);
-
   
-  int output[TOT_DIST] = {0};
-  int index = 0;
-  for (int i = 0; i < nr_cells-1; ++i){
-    size_t nr_read = fread(block, cell_byte, 1, file);
-    sscanf(block, "%lf %lf %lf", &x1, &y1, &z1);
+
+  int max_nr_cells = 10;
+  if (nr_cells < max_nr_cells)
+    max_nr_cells = nr_cells;
+  int nr_blocks = nr_cells / max_nr_cells + 1; // COulb be error here
+
+  //short* cell_block = (short*)aligned_alloc(64, sizeof(short*) * max_nr_cells * 3);
+  //short** matrix_block = (short**)aligned_alloc(64, sizeof(short*) * max_nr_cells);
+  short* cell_block = (short*)malloc(sizeof(short*) * max_nr_cells * 3);
+  short** matrix_block = (short**)malloc(sizeof(short*) * max_nr_cells);
+  short base_cell[3] = {0, 0, 0};
+
+  for (size_t i = 0; i < max_nr_cells; i++)
+    matrix_block[i] = cell_block + i * 3;
+  
+  short base_cell[3] = {0, 0, 0};
+  for(int i=0; i<max_nr_cells; i++){
+    char cell[cell_byte+1];
+    fseek(file, i*cell_byte, SEEK_SET);
+    fgets(cell, cell_byte+1, file);
+    base_cell = parse(cell);
+    for (int j=0; j<nr_blocks; j++){
+      for (int k=0; k<max_nr_cells; k++){
+        fgets(cell, cell_byte+1, file);
+        matrix[i][j] = parse(cell);
+      }
+      compute_distance(base_cell, matrix[i][j]);
+    }
     
-    for (int j = i+1; j < nr_cells; j += NR_READ){
-      size_t nr_cells_read = fread(block, cell_byte, NR_READ, file);
-      cell_ptr = block;
-
-        for (int k = 0; k < nr_cells_read; k++){
-
-          sscanf(cell_ptr, "%lf %lf %lf", &x2, &y2, &z2);
-
-
-          double distance = (sqrt(pow(x2-x1,2)+pow(y2-y1,2)+pow(z2-z1,2))*100);
-          int dist = (int)round(distance);
-          //(int)(sqrt(dist) * 100.0f + 0.5f);
-          printf("Distance: %d\n", dist);
-          printf("(%lf %lf %lf) (%lf %lf %lf)\n", x1, y1, z1, x2, y2, z2);
-          output[dist] += 1;
-          cell_ptr += cell_byte;
-          index++;
-        }
     }
-    // DIFFERENT LOCALLY AND ON GANTENB. Exclude + i+1 in ganten.
-    fseek(file, (i+1)*cell_byte + i+1, SEEK_SET);
-  }
-  printf("Index: %d\n", index);
-  printf("x1: %lf, y1: %lf, z1: %lf\n", x1, y1, z1);
-  printf("x2: %lf, y2: %lf, z2: %lf\n", x2, y2, z2);
-  fclose(file);
-  free(block);
-  printf("Data read.\n");
 
-  // Print output
-  printf("Printing output...\n");
-  for (int i = 0; i < TOT_DIST; ++i){
-    if (output[i] != 0){
-      printf("%.2f %i\n", i / 100.0, output[i]);
+  // Print all numbers in matrix 
+  printf("Printing matrix...\n");
+  for (int i=0; i<max_nr_cells; ++i){
+    for (int j=0; j<3; ++j){
+      printf("%hd ", matrix_block[i][j]);
     }
+    printf("\n");
   }
 
   return 0;
 }
-///////////////////////////////////////////////////////////////////////////////////////////
 
 
-int setMPThreadNumber(int argc, char *argv[], int *MP_threads) {
-  int option;
-  while ((option = getopt(argc, argv, "t:")) != -1)
-    if (option == 't') 
-      *MP_threads = atoi(optarg);
 
-  if (*MP_threads == -1) {
-    fprintf(stderr, "You must provide a value for number of threads ex: -t4\n");
-    exit(EXIT_FAILURE);
-  }
-  //omp_set_num_threads(MP_threads);
-  return 0;
+void parse(){
+      for (int j=0; j<3; ++j){
+      char num[7];
+      num[0] = cell[8*j + 0];
+      num[1] = cell[8*j + 1];
+      num[2] = cell[8*j + 2];
+
+      num[3] = cell[8*j + 4];
+      num[4] = cell[8*j + 5];
+      num[5] = cell[8*j + 6];
+      num[6] = '\0';
+      printf("Cell first: %s\n", num[0]);
+      printf("Cell second: %s\n", num[1]);
+
+      printf("%hd\n", (short)atoi(num));
+      matrix_block[i][j] = (short)atoi(num); // TODO: Find a better way of doing this
 }
