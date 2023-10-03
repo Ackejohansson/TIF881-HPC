3b18b779ac86df18111e5b5f6a7ed41a81879a3c
Axel <axelgbg@live.se>
2023-10-03 14:17:37 +0200

    WORKS 350ms for 1e3

--- A2/cell_distances.c
+++ A2/cell_distances.c
@@ -29,63 +29,63 @@ int main(int argc, char *argv[]) {
   }
   fseek(file, 0, SEEK_END);
   long file_size = ftell(file);
-  const size_t cell_byte = 24; //sizeof("+00.000 -00.000 +00.000");
+  const size_t cell_byte = 24;
   size_t nr_cells = file_size / cell_byte;
   printf("Number of cells: %zu\n", nr_cells);
   
   // Allocate memory
+  printf("Allocating memory...");
   int cells_read_max = 10;
   if (nr_cells < cells_read_max)
     cells_read_max = nr_cells;
-  int nr_blocks = ceil(nr_cells / cells_read_max);
-  int matrix_block[cells_read_max][3];
-  int base_cell[3] = {0, 0, 0};
-
-
+  int nr_blocks = ceil((double)nr_cells / cells_read_max);
+  double matrix_block[cells_read_max][3];
+  for (int i=0; i<cells_read_max; i++){
+    matrix_block[i][0] = 0.0;
+    matrix_block[i][1] = 0.0;
+    matrix_block[i][2] = 0.0;
+  }
+  double xBase, yBase, zBase;
+  double xDummy, yDummy, zDummy;
+  int distances[TOT_DIST]={0};
+  printf("Number of blocks: %d\n", nr_blocks);
+  int counter=0;
+  // Read data from file
+  printf("Reading data...\n");
   for(int i=0; i<cells_read_max; i++){
-    char cell[cell_byte+1];
-    fscanf(file, "%d %d %d", &base_cell[0], &base_cell[1], &base_cell[2]);
-    for (int j=0; j<nr_blocks; j++){
-      for (int k=0; k<cells_read_max; k++){
-        fscanf(file, "%hd %hd %hd", &matrix_block[k][0], &matrix_block[k][1], &matrix_block[k][2])
+    fseek(file, i*cell_byte +i+1, SEEK_SET);
+    fscanf(file, "%lf %lf %lf", &xBase, &yBase, &zBase);
+    for (int j = i+1; j < nr_cells; j += cells_read_max){
+      int index = 0;
+      while(index < cells_read_max && fscanf(file, "%lf %lf %lf", &xDummy, &yDummy, &zDummy) != EOF){
+        matrix_block[index][0] = xDummy;
+        matrix_block[index][1] = yDummy;
+        matrix_block[index][2] = zDummy;
+        ++index;
       }
-      for (int k=0; k<cells_read_max; k++){
-        int sq_xdiff = (matrix_block[k][0]-base_cell[0])*(matrix_block[k][0]-base_cell[0]);
-        int sq_ydiff = (matrix_block[k][1]-base_cell[1])*(matrix_block[k][1]-base_cell[1]);
-        int sq_zdiff = (matrix_block[k][2]-base_cell[2])*(matrix_block[k][2]-base_cell[2]);
-        int dist = (int)sqrtf(sq_xdiff + sq_ydiff + sq_zdiff)*100.0f + 0.5f;
+      // Calculate distances
+      //printf("Calculating distances...\n");
+      for (int k=0; k<index; k++){ // cant start on 0?
+        double sq_xdiff = (matrix_block[k][0]-xBase)*(matrix_block[k][0]-xBase);
+        double sq_ydiff = (matrix_block[k][1]-yBase)*(matrix_block[k][1]-yBase);
+        double sq_zdiff = (matrix_block[k][2]-zBase)*(matrix_block[k][2]-zBase);
+        double distance = sqrt(sq_xdiff + sq_ydiff + sq_zdiff)*100;
+        //printf("Distance: %f\n", distance);
+        int dist = (int)round(distance);
+        //printf("Distance: %d\n", dist);
+        //printf("Difference done...\n");
+        ++distances[dist];
+        ++counter;
       }
     }
   }
-
-  // Print all numbers in matrix 
-  printf("Printing matrix...\n");
-  for (int i=0; i<cells_read_max; ++i){
-    for (int j=0; j<3; ++j){
-      printf("%d ", matrix_block[i][j]);
-    }
-    printf("\n");
-  }
-
+  fclose(file);
+  printf("Printing results...\n");
+  // Print value of distances
+  
+  for (int i=0; i<TOT_DIST; i++)
+    if(distances[i] != 0)
+      printf("%d %d\n", i, distances[i]);
+  printf("Counter: %d\n", counter);
   return 0;
-}
-
-
-
-void parse(){
-      for (int j=0; j<3; ++j){
-      char num[7];
-      num[0] = cell[8*j + 0];
-      num[1] = cell[8*j + 1];
-      num[2] = cell[8*j + 2];
-
-      num[3] = cell[8*j + 4];
-      num[4] = cell[8*j + 5];
-      num[5] = cell[8*j + 6];
-      num[6] = '\0';
-      printf("Cell first: %s\n", num[0]);
-      printf("Cell second: %s\n", num[1]);
-
-      printf("%hd\n", (int)atoi(num));
-      matrix_block[i][j] = (int)atoi(num); // TODO: Find a better way of doing this
 }
